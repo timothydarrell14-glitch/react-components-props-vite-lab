bf2b940ba35874dbd664e70802cf8f91c0deec56
timothydarrell14-glitch <timothydarrell14@gmail.com>
2026-05-01 19:18:00 +0300

    Refactor Article and Three components to include return statement

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-function Article(props){
-    <>
+function Article(){
+    return (
+        <>
     <article>
     <h3></h3>
     <small></small>
     <p></p>
     </article>
     </>
+    )
 }
 
 export default Article
--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Three(props){
-
+    return (
+        <>
+    <article id={props.post.id}>
+    <h3>{props.post.title}</h3>
+    <small>{props.post.date}</small>
+    <p></p>
+    </article>
+    </>
+    )
 }
 
 export default Three 
